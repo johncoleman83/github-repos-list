5ff5d9702c32f1ec7032bc864f6e45b814b5cca5
John Coleman <jcoleman@pagerduty.com>
2018-10-20 21:48:14 -0700

    reformated date and fixed index count iteration

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,22 +5,28 @@ function generateTemplate (r) {
     '<div class="card-panel grey lighten-4 z-depth-4">',
     '<div class="row valign-wrapper">',
     "<div class='col s12 m6 l3 xl3'>",
-    "<a class='thumbnail' target='_blank' href='" + r.html_url + "'>",
+    "<a target='_blank' href='" + r.html_url + "'>",
     r.full_name,
     '</a>',
     '<div class="row">',
     "<div class='col s12 m12 l12 xl12'>",
       '<p><strong>' + r.description + '</strong></p>',
+      getHomepage(r),
       '<p>Languages: ' + getLanguages(r.languages_url) + '</p>',
-      '<p>Updated At: ' + r.updated_at + '</p>',
-      getWebsite(r),
+      '<p>Updated At: ' + getDateFormat(r.updated_at) + '</p>',
     '</div></div></div></div></div>',
   ].join('');
 }
 
-function getWebsite (repo) {
+function getDateFormat(date) {
+  let dateObj = new Date(date);
+  return dateObj.toDateString();
+}
+
+function getHomepage (repo) {
   if (repo.homepage && typeof repo.homepage === "string" && repo.homepage.length > 0) {
-    return '<p>Homepage: ' + repo.homepage + '</p>';
+    let href = "<a target='_blank' href='" + repo.homepage + "'>" + repo.homepage + '</a>';
+    return '<p>Homepage: ' + href + '</p>';
   }
   return '';
 }
@@ -45,7 +51,6 @@ var getData = (function ($) {
   
         let template = generateTemplate(r);
         if (i % 4 == 0) {
-
           $('#repositories').append(
             '<div class="row">' + template + '</div>'
           );
@@ -53,6 +58,7 @@ var getData = (function ($) {
           $('#repositories').append(template);
         }
         repos.push(r.html_url)
+        i += 1;
       });
     },
     error: function (data) {
