1ef4396b8c0e1efa771769b9078fe399572567fa
John Coleman <jcoleman@pagerduty.com>
2018-10-20 21:23:57 -0700

    adding materialize cards

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,6 +2,8 @@ const SUBJECT = 'johncoleman83'
 
 function generateTemplate (r) {
   return [
+    '<div class="card-panel grey lighten-4 z-depth-4">',
+    '<div class="row valign-wrapper">',
     "<div class='col s12 m6 l3 xl3'>",
     "<a class='thumbnail' target='_blank' href='" + r.html_url + "'>",
     r.full_name,
@@ -12,7 +14,7 @@ function generateTemplate (r) {
       '<p>Languages: ' + getLanguages(r.languages_url) + '</p>',
       '<p>Updated At: ' + r.updated_at + '</p>',
       getWebsite(r),
-    '</div></div></div>',
+    '</div></div></div></div></div>',
   ].join('');
 }
 
@@ -35,14 +37,21 @@ var getData = (function ($) {
     url: ReposUrl,
     type: 'GET',
     success: function (data) {
+      let i = 0;
       data.forEach(function (r) {
         if (!r || !r.id) {
           return true
         }
   
         let template = generateTemplate(r);
-  
-        $('#repositories').append(template)
+        if (i % 4 == 0) {
+
+          $('#repositories').append(
+            '<div class="row">' + template + '</div>'
+          );
+        } else {
+          $('#repositories').append(template);
+        }
         repos.push(r.html_url)
       });
     },
