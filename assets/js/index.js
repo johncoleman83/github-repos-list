24c0b09e70a248a25f5dc1d58fff38fcd06e8dc6
John Coleman <jcoleman@pagerduty.com>
2018-10-20 20:06:01 -0700

    reorganized directories, switched from bootstrap to materialize, brought in jquery

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,15 +2,16 @@ const SUBJECT = 'johncoleman83'
 
 function generateTemplate (r) {
   return [
-    "<div class='col-md-3 col-xs-6 col-lg-3' id='repository'><div class='container-fluid'>",
+    "<div class='col s12 m6 l3 xl4'>",
     "<a class='thumbnail' target='_blank' href='" + r.html_url + "'>",
     r.full_name,
     '</a>',
-    "<div class='caption'>",
+    '<div class="row">',
+    "<div class='col s12 m12 l12 xl12'>",
       '<strong>' + r.description + '</strong>',
       '<p> Languages: ' + getLanguages(r.languages_url) + '</p>',
     '</div></div></div>',
-  ].join();
+  ].join('');
 }
 
 function getLanguages (url) {
